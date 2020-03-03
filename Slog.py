b8c6ad6482a11169c3e59eb841dcc39443e29190
Lex <luohai2233@163.com>
2020-03-03 10:23:44 +0800

    change log level

--- Slog.py
+++ Slog.py
@@ -8,7 +8,7 @@ _g_logger = None
 def get_logger():
     global _g_logger
     if _g_logger is None:
-        logging.basicConfig(filename=LOG_FILE)
+        logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
         __datetime = "%Y-%m-%d %H:%M:%S"
         __format_str = "%(asctime)s %(levelname).1s [%(filename)s:%(lineno)s] %(message)s"
         _g_logger = logging.getLogger()
@@ -17,7 +17,6 @@ def get_logger():
         handler = logging.StreamHandler()
         handler.setFormatter(formater)
         _g_logger.addHandler(handler)
-        _g_logger.setLevel(logging.DEBUG)
     return _g_logger
 
 
