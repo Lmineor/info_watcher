83774e89d437d5e6df82d81bfd9a1a814faad19d
Lex <luohai2233@163.com>
2020-04-15 12:41:10 +0800

    update: 增加模块化程度

--- config.py
+++ config.py
@@ -13,12 +13,48 @@ except ImportError as e:
 SENDER = MAIL_USER
 RECEIVERS = [MAIL_USER]
 
+
+# 目标院校
+SCHOOLS = {
+    '自动化所': 'http://www.ia.cas.cn/yjsjy/zs/sszs/index.html',
+    '计算所': 'http://www.ict.cas.cn/shye/tzgg/index.html',
+    '信工所': 'http://www.iie.cas.cn/yjsjy_101173/yjszxtz/index.html',
+    '网络信息中心': 'http://www.cnic.cas.cn/yjsjy/tzgg/index.html',
+    '声学所': 'http://www.ioa.cas.cn/yjsjy/zs/zstz/',
+    '国科大招生网站': 'http://admission.ucas.ac.cn/',
+    '考研帮': 'http://tiaoji.kaoyan.com/beijing/',
+}
+
+
+
+"""
+li_list = soup.find('ul', attrs={'class':'fontlist'})
+    re_pattern = '(?<=>).+?(?=<)'
+    res = []
+    for li in li_list:
+        title = re.findall(re_pattern, str(li.find('a')))
+        if title:
+            res.append(title[0])
+    return res
+"""
+RULES = {
+    '自动化所': ['ul', 'fontlist', 'find'],       # ['label', 'attrs', 'findall or find or fail']
+    '计算所': ['td', 'black_14', 'findAll'],
+    '信工所': ['td', 'black_14', 'findAll'],
+    '网络信息中心': ['ul', 'list-font margin-top-10 clearfix', 'find'],
+    '声学所': ['', '', 'fail'],
+    '国科大招生网站': ['ul', 'mp-list', 'find'],
+    '考研帮': ['ul', 'list areaZslist', 'find'],
+}
+
 # 目标院校
 ZDH = 'http://www.ia.cas.cn/yjsjy/zs/sszs/index.html' # 自动化所
 JSS = 'http://www.ict.cas.cn/shye/tzgg/index.html' # 计算所
 XGS = 'http://www.iie.cas.cn/yjsjy_101173/yjszxtz/index.html' # 信工所
 WLXXZX = 'http://www.cnic.cas.cn/yjsjy/tzgg/index.html' # 网络信息中心
 SXS = 'http://www.ioa.cas.cn/yjsjy/zs/zstz/' # 声学所
+GKD = 'http://admission.ucas.ac.cn/' # 国科大招生网站
+
 
 # 第三方网站
 KYB = 'http://tiaoji.kaoyan.com/beijing/' # 考研帮
--- spider.py
+++ spider.py
@@ -5,107 +5,52 @@ from typing import List
 import requests
 from bs4 import BeautifulSoup
 
-from config import ZDH, JSS, XGS, WLXXZX, KYB, SXS
-from send_email import send_email
+from config import SCHOOLS, RULES
 from Slog import logger
 
-def get_info_zdh() -> list:
-    # 自动化所信息
-    logger.info("开始查询自动化所招生信息")
-    rep = get_req(ZDH)
-    soup = BeautifulSoup(rep.text, 'html.parser')
-    li_list = soup.find('ul', attrs={'class':'fontlist'})
-    re_pattern = '(?<=>).+?(?=<)'
-    res = []
-    for li in li_list:
-        title = re.findall(re_pattern, str(li.find('a')))
-        if title:
-            res.append(title[0])
-    return res
 
-def get_info_jss() -> list:
-    # 计算所信息
-    logger.info("开始查询计算所招生信息")
-    rep = get_req(JSS)
-    soup = BeautifulSoup(rep.text, 'html.parser')
-    li_list = soup.findAll('td', attrs={'class':'black_14'})
-    re_pattern = '(?<=>).+?(?=<)'
-    res = []
-    for li in li_list:
-        title = re.findall(re_pattern, str(li.find('a')))
-        if title:
-            res.append(title[0])
-    return res
+def __drink_soup(url, parser = 'html.parser'):
+    req = requests.get(url, timeout=100)
+    req.encoding = 'utf-8'
+    soup = BeautifulSoup(req.text, 'html.parser')
+    return soup
 
 
-
-def get_info_xgs() -> list:
-    # 信工所信息
-    logger.info("开始查询信工所招生信息")
-    rep = get_req(XGS)
-    soup = BeautifulSoup(rep.text, 'html.parser')
-    li_list = soup.findAll('td', attrs={'class':'black_14'})
-    re_pattern = '(?<=>).+?(?=<)'
-    res = []
-    for li in li_list:
-        title = re.findall(re_pattern, str(li.find('a')))
-        if title:
-            res.append(title[0])
-    return res
-
-def get_info_wlxxzx() -> list:
-    # 网络信息中心信息
-    logger.info("开始查询网络信息中心招生信息")
-    rep = get_req(WLXXZX)
-    soup = BeautifulSoup(rep.text, 'html.parser')
-    li_list = soup.find('ul', attrs={'class':'list-font margin-top-10 clearfix'})
-    re_pattern = '(?<=>).+?(?=<)'
-    res = []
-    for li in li_list:
-        title = re.findall(re_pattern, str(li.find('a')))
-        if title:
-            res.append(title[0])
-    return res
+def __get_news(school) -> list:
+    if RULES[school][2] == 'fail':
+        return ''
+    else:
+        logger.info("开始查询{}招生信息".format(school))
+        soup = __drink_soup(SCHOOLS[school])
+        if RULES[school][2] == 'find':
+            li_list = soup.find(RULES[school][0], attrs={'class':RULES[school][1]})
+        elif RULES[school][2] == 'findAll':
+            li_list = soup.findAll(RULES[school][0], attrs={'class':RULES[school][1]})
+        re_pattern = '(?<=>).+?(?=<)'
+        res = []
+        for li in li_list:
+            title = re.findall(re_pattern, str(li.find('a')))
+            if title:
+                res.append(title[0])
+        return res
 
 
-def get_info_kyb() -> list:
-    # 考研帮信息查询
-    logger.info("开始查询考研帮招生信息")
-    rep = get_req(KYB)
-    soup = BeautifulSoup(rep.text, 'html.parser')
-    li_list = soup.find('ul', attrs={'class':'list areaZslist'})
-    re_pattern = '(?<=>).+?(?=<)'
-    res = []
-    for li in li_list:
-        title = re.findall(re_pattern, str(li.find('a')))
-        if title:
-            res.append(title[0])
-    return res
+def get_info_from_schools() -> list:
+    school_news = []
+    for school, url in SCHOOLS.items():
+        school_news.append(school)
+        school_news.append(url)
+        school_news.append('')
+        school_news.append('')
+        news = __get_news(school)
+        school_news += news
+        school_news.append('')
+        school_news.append('')
+    return school_news
 
-def get_info_sxs() -> list:
-    # 自动化所信息
-    logger.info("开始查询声学所所招生信息")
-    req = get_req(SXS)
-    soup = BeautifulSoup(rep.text, 'html.parser')
-    trs = soup.find_all('table')
-
-    table_info = soup.select('body>table:nth-of-type(6)>tbody>tr>td:nth-of-type(3)>table:nth-of-type(2)>tbody>tr>td>table:nth-of-type(3)>tbody>tr')
-    print(table_info)
-    'body > table:nth-child(6) > tbody > tr > td:nth-child(3) > table:nth-child(2) > tbody > tr > td > table:nth-child(3) > tbody > tr:nth-child(1)'
-    # re_pattern = '(?<=>).+?(?=<)'
-    # 'body > table:nth-child(6) > tbody > tr > td:nth-child(3) > table:nth-child(2) > tbody > tr > td > table:nth-child(3)'
-    # res = []
-    # for li in li_list:
-    #     title = re.findall(re_pattern, str(li.find('a')))
-    #     if title:
-    #         res.append(title[0])
-    # return res
-
-
-def get_req(url):
-    rep = requests.get(KYB, timeout=5)
-    rep.encoding = 'utf-8'
-    return rep
-
+        
 if __name__ == '__main__':
-    print(get_info_kyb())
+    # print(get_info_gkd())
+    # for school, url in SCHOOLS.items():
+    #     print(RULES[school][1])
+    print(get_info_from_schools())
