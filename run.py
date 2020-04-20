35523fc04481e3750ae854997af55b767ffac43a
Lex <luohai2233@163.com>
2020-04-20 17:06:45 +0800

    update: 有diff就返回，不处理diff

--- run.py
+++ run.py
@@ -3,23 +3,24 @@ from typing import List
 import asyncio
 
 from Slog import logger
-from config import INTERVAL
+from config import INTERVAL, SCHOOLS, RULES
 from spider import get_info_from_schools
 from send_email import send_email
 
 
+
 logger.info("Start run.py")
 
-schoo_old_news = []
+school_old_news = []
 def run():
-    global schoo_old_news
+    global school_old_news
     try:
-        schoo_new_news = get_info_from_schools()
+        school_new_news = get_info_from_schools()
     except Exception as e:
         logger.error(e)
-        schoo_new_news = ['']
-    if diff(schoo_new_news, schoo_old_news):
-        _diff = diff(schoo_new_news, schoo_old_news)
+        school_new_news = ['']
+    if not diff(school_new_news, school_old_news):
+        _diff = school_new_news
     else:
         _diff = []
     if '\n'.join(_diff):
@@ -28,14 +29,15 @@ def run():
         to = '雒海艇'
         subject = "招生信息更新"
         send_email(contend, froms, to, subject)
-    schoo_old_news = schoo_new_news
+    school_old_news = school_new_news
 
 
 def diff(new_info, old_info) -> list:
-    if not old_info:
-        return new_info
-    else:
-        return new_info[:new_info.index(old_info[0])]
+    return new_info == old_info
+    # if not old_info:
+    #     return new_info
+    # else:
+    #     return new_info[:new_info.index(old_info[0])]
 
 
 if __name__ == '__main__':
