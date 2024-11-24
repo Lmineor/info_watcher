a680845b903a893da21577acafe9accd69e520e9
lex <luohai2233@163.com>
2024-11-24 14:50:23 +0800

    update

--- info_watcher/spider/Youzheng.py
+++ info_watcher/spider/Youzheng.py
@@ -2,9 +2,6 @@
 # -*- coding: utf-8 -*-
 import os
 from typing import List
-from selenium import webdriver
-from selenium.webdriver.edge.service import Service as EdgeService
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 from info_watcher.spider.base import Spider
 from info_watcher.logger.logger import logger
@@ -25,14 +22,11 @@ class YouZhengSpider(Spider):
     def extract(self) -> List:
         results = []
         logger.info("开始检测{}".format(self.url))
-
-        driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
-        driver.get("https://mall.11185.cn/web/searchJy?businessId=JY&prodGoodsType=NORMAL&classficationId=JY-0")
-        soup = self.drink_soup(driver.page_source)
+        soup = self.get_soup(self.url)
         detail_body = soup.find('div', class_='detail_center')
         card_con1s = detail_body.find_all('div', class_='card_con1')
         for card_con1 in card_con1s:
             if card_con1:
                 results.append(card_con1.get_text(strip=True))
-        driver.quit()
+
         return results
--- info_watcher/spider/base.py
+++ info_watcher/spider/base.py
@@ -4,6 +4,10 @@ import os
 from typing import List
 
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 from info_watcher.config import OLD_INFO_DIR
 
@@ -46,6 +50,17 @@ class Spider(object):
         diffs = self.compare_new_old(new_infos, old_infos)
         self.write_new_info(self.filename, new_infos)
         return diffs
+
+    def get_soup(self, url):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')  # 无头模式运行，不需要图形界面
+        chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片，提升速度
+        chrome_options.add_argument('--disable-gpu')  # 禁用GPU加速
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        driver.get(url)
+        page_source = driver.page_source
+        driver.quit()
+        return self.drink_soup(page_source)
 
 
 def diff(new_info: List, old_info: List) -> List:
